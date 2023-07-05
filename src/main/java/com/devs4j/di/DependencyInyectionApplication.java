97ce5d73203d6eea239a0e25ba821491c2bf260c
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2023-07-05 11:47:57 -0300

    12. Uso de Qualifiers

--- src/main/java/com/devs4j/di/DependencyInyectionApplication.java
+++ src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -1,23 +1,38 @@
 package com.devs4j.di;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.devs4j.di.atributo.Coche;
-import com.devs4j.di.atributo.Motor;
+import com.devs4j.di.qualifiers.Animal;
+import com.devs4j.di.qualifiers.Avion;
+import com.devs4j.di.qualifiers.Nido;
+import com.devs4j.di.qualifiers.Pajaro;
+import com.devs4j.di.qualifiers.Perro;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-		Coche coche = context.getBean(Coche.class);
 		
-//		Motor motor = new Motor("Xl1", 1981);
-//		Coche coche = new Coche("VW", 1986, motor);
+		Perro perro = context.getBean(Perro.class);
+		log.info("Objeto perro {}", perro.getNombre());
 		
-		System.out.println(coche);
+		Pajaro pajaro = context.getBean(Pajaro.class);
+		log.info("Objeto pajaro {}", pajaro.getNombre());
+		
+		Avion avion = context.getBean(Avion.class);
+		avion.volar();
+		
+		Animal animal = context.getBean("pajarito", Animal.class);
+		log.info("Animal nombre = {}, edad = {}", animal.getNombre(), animal.getEdad());
+		
+		Nido nido = context.getBean(Nido.class);
+		nido.imprimir();
 	}
-
 }
