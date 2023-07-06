4d3546e001b51e893b9bea1f6e3e626eeb39b7c1
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2023-07-06 15:17:07 -0300

    27. Interfaces InitializingBean y DisposableBean

--- src/main/java/com/devs4j/di/lifecycle/LifeCycleBean.java
+++ src/main/java/com/devs4j/di/lifecycle/LifeCycleBean.java
@@ -3,18 +3,22 @@ package com.devs4j.di.lifecycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
-@Scope("prototype")
-public class LifeCycleBean implements BeanNameAware {
+//@Scope("prototype") -- Recordemos que PreDestroy no se ejecuta para beans de tipo prototype
+public class LifeCycleBean implements BeanNameAware, InitializingBean, DisposableBean {
 	
 	private static final Logger log = LoggerFactory.getLogger(LifeCycleBean.class);
 
+	/*
+	 * Se ejecuta durante la construccion del bean "BeanNameAware"
+	 */
 	@Override
 	public void setBeanName(String name) {
 		log.info("Bean name aware {}", name);
@@ -24,7 +28,7 @@ public class LifeCycleBean implements BeanNameAware {
 	 * Se ejecutara despues de la inyeccion de dependencias
 	 */
 	@PostConstruct
-	public void init() {
+	public void initBean() {
 		log.info("Post Construct");
 	}
 	
@@ -34,7 +38,17 @@ public class LifeCycleBean implements BeanNameAware {
 	 * Solo se ejecutan durante una terminacion del la VM de forma normal
 	 */
 	@PreDestroy
-	public void destroy() {
+	public void destroyBean() {
 		log.info("Pre Destroy");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		log.info("After properties set method");
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		log.info("Destroy method");
 	}
 }
