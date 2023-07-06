d986bc9be88fe3cad56ef5602218cbb29ab29b1a
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2023-07-06 15:03:57 -0300

    26. Reglas al utilizar PostConstruct y PreDestroy

--- src/main/java/com/devs4j/di/lifecycle/LifeCycleBean.java
+++ src/main/java/com/devs4j/di/lifecycle/LifeCycleBean.java
@@ -3,9 +3,14 @@ package com.devs4j.di.lifecycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class LifeCycleBean implements BeanNameAware {
 	
 	private static final Logger log = LoggerFactory.getLogger(LifeCycleBean.class);
@@ -14,5 +19,22 @@ public class LifeCycleBean implements BeanNameAware {
 	public void setBeanName(String name) {
 		log.info("Bean name aware {}", name);
 	}
-
+	
+	/*
+	 * Se ejecutara despues de la inyeccion de dependencias
+	 */
+	@PostConstruct
+	public void init() {
+		log.info("Post Construct");
+	}
+	
+	/*
+	 * Se ejecutara antes de que el bean sea destruido
+	 * No se ejecutan para beans prototype
+	 * Solo se ejecutan durante una terminacion del la VM de forma normal
+	 */
+	@PreDestroy
+	public void destroy() {
+		log.info("Pre Destroy");
+	}
 }
