170b4724a16aa333de17681bbd354f03fe05a33a
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2023-07-06 14:38:56 -0300

    24. Aware Interfaces

--- src/main/java/com/devs4j/di/DependencyInyectionApplication.java
+++ src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -1,25 +1,17 @@
 package com.devs4j.di;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.devs4j.di.autowire.AreaCalculatorService;
-
+import com.devs4j.di.lifecycle.LifeCycleBean;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
-	
-	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
-
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
 		
-		AreaCalculatorService calculator = context.getBean(AreaCalculatorService.class);
-
-		log.info("Area total {}", calculator.calcAreas());
+		context.getBean(LifeCycleBean.class);
 	}
 }
