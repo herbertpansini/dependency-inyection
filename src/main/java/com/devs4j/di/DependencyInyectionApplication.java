674d26a86cd7ea05ff4c33ef28f4ddb22143cb2c
Herbert <hebert.aggrandize@fornecedores.klabin.com.br>
2023-07-06 10:18:12 -0300

    18. Definicion de beans de forma explicita

--- src/main/java/com/devs4j/di/DependencyInyectionApplication.java
+++ src/main/java/com/devs4j/di/DependencyInyectionApplication.java
@@ -5,21 +5,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-import com.devs4j.di.scopes.EjemploScopeService;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
 
+    @Bean
+    String getApplicationName() {
+        return "Devs4j rules!";
+    }
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
 		
-		EjemploScopeService ejemploScopeService = context.getBean(EjemploScopeService.class);
-		EjemploScopeService ejemploScopeService1 = context.getBean(EjemploScopeService.class);
+		String nombreAplicacion = context.getBean(String.class);
 		
-		log.info("Are beans equals {}", ejemploScopeService.equals(ejemploScopeService1));
-		log.info("Are beans == {}", ejemploScopeService == ejemploScopeService1);
+
+		log.info("Nombre aplicacion {}", nombreAplicacion);
 	}
 }
